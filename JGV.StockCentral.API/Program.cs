deb9b79ce593cc5a2a901448b6fcac565f774264
João V Garcia <joao.garcia.vitor@hotmail.com>
2023-10-29 17:03:04 -0300

    Adds GetAll endpoints for Products, Clients and Sells

--- JGV.StockCentral.API/Program.cs
+++ JGV.StockCentral.API/Program.cs
@@ -1,7 +1,16 @@
+using JGV.StockCentral.API;
+using JGV.StockControl.Library.DAL;
+using JGV.StockControl.Library.DAL.IRepository;
+using JGV.StockControl.Library.DAL.Models;
+using JGV.StockControl.Library.DAL.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
@@ -9,5 +18,18 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.MapGet("/Clientes", (IUnitOfWork db) =>
+{
+    return Results.Ok(db.ClientRepository.GetAll());
+});
+app.MapGet("/Produtos", (IUnitOfWork db) =>
+{
+    return Results.Ok(db.ProductRepository.GetAll());
+});
+app.MapGet("/Vendas", (IUnitOfWork db) =>
+{
+    return Results.Ok(db.SellRepository.GetAll());
+});
 
 app.Run();
---
+++ JGV.StockCentral.API/ServicesConfig.cs
@@ -0,0 +1,27 @@
+﻿using JGV.StockControl.Library.DAL.IRepository;
+using JGV.StockControl.Library.DAL.Models;
+using JGV.StockControl.Library.DAL.Repository;
+using JGV.StockControl.Library.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace JGV.StockCentral.API
+{
+    public static class ServicesConfig
+    {
+        public static void ConfigureServices(this WebApplicationBuilder webApplicationBuilder)
+        {
+            var services = webApplicationBuilder.Services;
+
+            string dbPath = Path.Combine(Directory.GetCurrentDirectory() + "\\bin\\Debug\\net6.0\\", "StockControlLocalDb.db");
+            services.AddDbContext<StockControlDbContext>(options => options.UseSqlite($"Data Source={dbPath}"));
+
+            services.AddTransient<IClientRepository, ClientRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<ISellRepository, SellRepository>();
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.BuildServiceProvider();
+        }
+    }
+}
